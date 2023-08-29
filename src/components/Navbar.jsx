dedc44a9de83e424e1e8d679c2762832a35273a8
Christopher Lynde <lyndechris@gmail.com>
2023-08-29 19:44:56 -0400

    Navbar style updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
-// Navbar.js
+// Navbar.jsx
 import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-black py-4">
-      <div className="container mx-auto text-white">
-        {/* Navigation links */}
+    <nav className="bg-black shadow-lg py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <div className="text-white text-xl font-semibold">Christopher Lynde</div>
+        <div className="flex space-x-6">
+          <a className="text-gray-300 hover:text-white transition duration-300 ease-in-out" href="#hero">Home</a>
+          <a className="text-gray-300 hover:text-white transition duration-300 ease-in-out" href="#portfolio">Portfolio</a>
+          <a className="text-gray-300 hover:text-white transition duration-300 ease-in-out" href="#contact">Contact</a>
+        </div>
       </div>
     </nav>
   );
