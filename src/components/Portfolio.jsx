10b55840e0d7bd957c376ed619c122db3d4e972f
Christopher Lynde <lyndechris@gmail.com>
2023-09-06 11:46:06 -0400

    Portfolio section style tweaks

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -9,21 +9,21 @@ const Portfolio = () => {
     {
       id: 1,
       title: 'Ajenda',
-      description: 'A description of Project 1 goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'Task management and scheduling app.',
       image: ajendaImg, 
       tools: ['React', 'Tailwind CSS', 'Firebase'],
     },
         {
       id: 3,
       title: 'Streamwake',
-      description: 'A description of Project 3 goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'Discord bot - Watchlist/Player/Activity Tracking.',
       image: streamwakeImg,
       tools: ['React', 'Node.js', 'Express'],
     },
     {
       id: 2,
       title: 'Ajenda Landing Page',
-      description: 'A description of Project 2 goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      description: 'Landing page for the Ajenda app.',
       image: ajendaLandingImg,
       tools: ['HTML', 'CSS', 'JavaScript'],
     },
@@ -33,11 +33,11 @@ const Portfolio = () => {
   return (
     <section id="portfolio" className="bg-smoke-500 py-20">
       <div className="container mx-auto">
-        <h2 className="text-4xl font-bold text-white mb-8">Portfolio</h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+        <h2 className="text-4xl text-white mb-8"><span className="italic bg-smoke-100 rounded-tr-lg rounded-bl-lg">Portfolio.</span></h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-24">
           {portfolioItems.map((item) => (
-            <div key={item.id} className="bg-smoke-100 rounded-lg overflow-hidden shadow-md">
-              <img src={item.image} alt={item.title} className="aspect-auto overflow-hidden p-2 rounded-t-xl" />
+            <div key={item.id} className="bg-smoke-100 rounded-lg overflow-hidden">
+              <img src={item.image} alt={item.title} className="w-full h-1/2 object-fill overflow-hidden p-2 rounded-t-xl" />
               <div className="p-4 flex flex-col">
                 <h3 className="text-white text-2xl font-semibold mb-2">{item.title}</h3>
                 <p className="text-white mb-4">{item.description}</p>
