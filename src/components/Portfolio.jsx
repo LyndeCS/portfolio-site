36a9e0c45d68e98d3e7189e31a93f8d2aa7a6bd0
Christopher Lynde <lyndechris@gmail.com>
2023-09-06 21:18:29 -0400

    Portfolio seciton v0.2
    Style updates for the cards in the portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,24 +8,24 @@ const Portfolio = () => {
   const portfolioItems = [
     {
       id: 1,
-      title: 'Ajenda',
-      description: 'Task management and scheduling app.',
-      image: ajendaImg, 
-      tools: ['React', 'Tailwind CSS', 'Firebase'],
-    },
-        {
-      id: 3,
-      title: 'Streamwake',
-      description: 'Discord bot - Watchlist/Player/Activity Tracking.',
-      image: streamwakeImg,
-      tools: ['React', 'Node.js', 'Express'],
+      title: 'Ajenda (Landing Page)',
+      description: 'Landing page to serve as the value proposition to use the Ajenda app.',
+      image: ajendaLandingImg,
+      tools: ['React', 'TypeScript', 'Tailwind'],
     },
     {
       id: 2,
-      title: 'Ajenda Landing Page',
-      description: 'Landing page for the Ajenda app.',
-      image: ajendaLandingImg,
-      tools: ['HTML', 'CSS', 'JavaScript'],
+      title: 'Ajenda',
+      description: 'Task management and scheduling app. Create tasks to be accompished, then schedule them in the calendar to stay organized.',
+      image: ajendaImg, 
+      tools: ['React', 'Firebase', 'MUI'],
+    },
+    {
+      id: 3,
+      title: 'Streamwake',
+      description: 'Discord bot -  Suggest shows to watch, admin selects watchlist from recently watched shows and suggested shows. User viewing activity is logged to keep track of episodes watched.',
+      image: streamwakeImg,
+      tools: ['JavaScript', 'Node.js', 'Discord.js'],
     },
 
   ];
@@ -33,19 +33,19 @@ const Portfolio = () => {
   return (
     <section id="portfolio" className="bg-smoke-500 py-20">
       <div className="container mx-auto">
-        <h2 className="text-4xl text-white mb-8"><span className="italic bg-smoke-100 rounded-tr-lg rounded-bl-lg">Portfolio.</span></h2>
+        <h2 className="text-4xl text-white mb-8"></h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-24">
           {portfolioItems.map((item) => (
-            <div key={item.id} className="bg-smoke-100 rounded-lg overflow-hidden">
-              <img src={item.image} alt={item.title} className="w-full h-1/2 object-fill overflow-hidden p-2 rounded-t-xl" />
-              <div className="p-4 flex flex-col">
-                <h3 className="text-white text-2xl font-semibold mb-2">{item.title}</h3>
-                <p className="text-white mb-4">{item.description}</p>
+            <div key={item.id} className="bg-smoke-100 rounded-lg h-[30rem] flex flex-col p-2">
+              <img src={item.image} alt={item.title} className="aspect-video overflow-hidden rounded-t-lg" />
+              <div className="flex flex-col justify-between h-3/4 pt-2 px-2">
+                <h3 className="text-white text-2xl my-2">{item.title}</h3>
+                <p className="text-gray-400 mb-auto">{item.description}</p>
                 <div className="flex flex-wrap">
                   {item.tools.map((tool, index) => (
                     <span
                       key={index}
-                      className="bg-gray-200 text-gray-700 text-sm rounded-full py-1 px-2 mr-2 mb-2"
+                      className="bg-gray-200 text-gray-700 text-sm font-medium rounded-full py-1 px-2 mr-2 mb-2"
                     >
                       {tool}
                     </span>
