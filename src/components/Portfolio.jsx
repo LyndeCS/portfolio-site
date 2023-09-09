b57f906eae434c484ad9d0d8897b9d9ba40ede4d
Christopher Lynde <lyndechris@gmail.com>
2023-09-09 18:57:52 -0400

    Portfolio darker green accent
    -Changed border of cards to meadow-500

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -45,11 +45,11 @@ const Portfolio = () => {
         <h2 className="text-2xl text-white mb-16 italic"><span className=""><span className="text-meadow-500">&lt;</span>Portfolio <span className="text-meadow-500">&#47;&gt;</span></span></h2>
         <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-24">
           {projects.map((item) => (
-            <div key={item.id} className="bg-smoke-500 shadow-sm border-t border-t-meadow-300 shadow-meadow-300 rounded-lg h-[30rem] flex flex-col p-2">
+            <div key={item.id} className="bg-smoke-500 shadow-sm border-t border-t-meadow-500 shadow-meadow-500 rounded-lg h-[30rem] flex flex-col p-2">
               <a href={item.link} target="_blank" rel="noopener noreferrer"><img src={item.image} alt={item.title} className="rounded-t-lg brightness-90 hover:brightness-100" /></a>
               <div className="flex">
-              <hr className="mt-2 bg-gradient-to-l from-meadow-300 to-smoke-500 h-[1px] w-1/2 border-none"></hr>
-              <hr className="mt-2 bg-gradient-to-r from-meadow-300 to-smoke-500 h-[1px] w-1/2 border-none"></hr>
+              <hr className="mt-2 bg-gradient-to-l from-meadow-500 to-smoke-500 h-[1px] w-1/2 border-none"></hr>
+              <hr className="mt-2 bg-gradient-to-r from-meadow-500 to-smoke-500 h-[1px] w-1/2 border-none"></hr>
               </div>
               <div className="flex flex-col justify-between h-2/3 pt-2 px-2">
                 <a href={item.link} target="_blank" rel="noopener noreferrer"><h3 className="text-white text-2xl mt-2 mb-4 hover:underline">{item.title}</h3></a>
