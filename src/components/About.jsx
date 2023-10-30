a65dfd3ba2e0be51ed98337f58129a81af7c943d
Christopher Lynde <lyndechris@gmail.com>
2023-10-29 20:41:14 -0400

    Mobile Responsive v0.3
    -About section sufficiently mobile responsive
    
    -lg screen size spacing between middle section and right section needs tweaking
    
    -lg screen size spacing between right section icons needs tweaking

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -43,15 +43,15 @@ const techIcons = [
 
 const About = () => {
 	return (
-		<section id="about" className="bg-smoke-500 pt-24">
-			<div className="container mx-auto flex justify-start">
+		<section id="about" className="bg-smoke-500 pt-4 md:pt-24">
+			<div className="container mx-auto flex justify-start px-4 md:px-0">
 				{/* Left section with image and social links */}
-				<div className="w-1/4">
+				<div className="w-1/4 h-min flex-shrink">
 					{/* Profile image */}
 					<img
 						src={headshot}
 						alt="Your Name"
-						className="rounded w-48 h-64 object-cover scale-x-[-1]"
+						className="rounded w-24 md:w-48 h-32 md:h-64 object-cover scale-x-[-1]"
 					/>
 
 					{/* Social media links */}
@@ -64,7 +64,7 @@ const About = () => {
 								href="https://github.com/LyndeCS"
 								className="text-white ml-4 hover:text-meadow-500"
 							>
-								Follow on GitHub
+								<span className="hidden md:inline-block">Follow on</span> GitHub
 							</a>
 						</div>
 						<div className="flex items-center">
@@ -75,7 +75,8 @@ const About = () => {
 								href="https://www.linkedin.com/in/chrislynde/"
 								className="text-white ml-4 hover:text-meadow-500"
 							>
-								Follow on LinkedIn
+								<span className="hidden md:inline-block">Follow on</span>{" "}
+								LinkedIn
 							</a>
 						</div>
 						<div className="flex items-center">
@@ -86,81 +87,97 @@ const About = () => {
 								href="https://twitter.com/lynde_chris"
 								className="text-white ml-4 hover:text-meadow-500"
 							>
-								Follow on Twitter
+								<span className="hidden md:inline-block">Follow on</span>{" "}
+								Twitter
 							</a>
 						</div>
-						<hr className="w-1/2 bg-gray-500 h-[1px] border-none opacity-50"></hr>
+
+						{/* DIVIDER */}
+						<hr className="w-[90px] md:w-[190px] bg-gray-500 h-[1px] border-none opacity-50"></hr>
+
 						<div className="flex items-center">
 							<FaEnvelope size={16} className="text-gray-500" />
 							<a
 								href="mailto:lyndechris@gmail.com"
 								className="text-white ml-4 hover:text-meadow-500"
 							>
-								lyndechris@gmail.com
+								<span className="hidden md:inline-block">
+									lyndechris@gmail.com
+								</span>
+								<span className="inline-block md:hidden">Email</span>
 							</a>
 						</div>
 					</div>
 				</div>
 
 				{/* Middle section with paragraphs */}
-				<div className="w-2/4">
-					<h1 className="text-4xl font-bold text-white mb-6">
-						My name is{" "}
-						<span className="text-meadow-500">Christopher Lynde</span>.
-					</h1>
-					<h1 className="text-4xl font-bold text-white mb-12">
-						I'm a full-stack developer with a passion for creating{" "}
-						<span className="underline italic decoration-meadow-500">
-							stylish and intuitive interfaces.
-						</span>
-					</h1>
-					<div className="text-white mb-6">
-						<p className="text-xl text-justify">
-							I believe in respecting the user.{" "}
-							<span className="italic decoration-meadow-500 bg-shark-300">
-								Fewer clicks, faster results.
-							</span>{" "}
-							In order to deliver on this principle, I'm driven to discover all
-							that is possible of the tools at my disposal. As a result, and in
-							addition to being naturally curious, I am excited to engage with
-							new technologies and take every opportunity to synthesize new
-							knowledge in order to build the best product imaginable.
-						</p>
+				<div className="flex-grow lg:w-2/4 pl-4 md:pl-8 lg:pl-0">
+					{/* HEADING/SUBHEADING */}
+					<div className="flex flex-col h-full justify-around md:h-min text-2xl xs:text-3xl sm:text-4xl sm:pl-4 md:pl-0">
+						<h1 className="font-bold text-white mb-6">
+							My name is{" "}
+							<span className="text-meadow-500">Christopher Lynde</span>.
+						</h1>
+						<h1 className="font-bold text-white md:mb-12">
+							I'm a full-stack developer with a passion for creating{" "}
+							<span className="underline italic decoration-meadow-500">
+								stylish and intuitive interfaces.
+							</span>
+						</h1>
 					</div>
 
-					<div className="text-white mb-6">
-						<p className="text-xl text-justify">
-							I graduated from the Web Development program at Fanshawe College
-							in 2021, where I learned the foundations of HTML5, CSS3,
-							JavaScript (ES6), LAMP (Linux, Apache, MySQL, PHP), and CMS
-							(WordPress). Since then, I've added React (up to v18), TypeScript,
-							Firebase DB (NoSQL), and Node to my repertoire, as well as
-							Bootstrap, MUI, and Tailwind for styling.
-						</p>
-					</div>
+					{/* DESKTOP PARAGRAPHS */}
+					<div className="hidden md:block flex-grow">
+						<div className="text-white mb-6">
+							<p className="text-lg lg:text-xl md:text-justify">
+								I believe in respecting the user.{" "}
+								<span className="italic decoration-meadow-500 bg-shark-300">
+									Fewer clicks, faster results.
+								</span>{" "}
+								In order to deliver on this principle, I'm driven to discover
+								all that is possible of the tools at my disposal. As a result,
+								and in addition to being naturally curious, I am excited to
+								engage with new technologies and take every opportunity to
+								synthesize new knowledge in order to build the best product
+								imaginable.
+							</p>
+						</div>
 
-					<div className="text-white mb-6">
-						<p className="text-xl text-justify">
-							I spend most of my time creating websites, apps, discord bots, you
-							name it, in order to improve my skills as a developer. I gravitate
-							towards building interfaces - I love finding ways to make
-							interactions as intuitive and accessible as possible while
-							maintaining a sleek and thematic style.
-						</p>
-					</div>
+						<div className="text-white mb-6">
+							<p className="text-lg md:text-xl md:text-justify">
+								I graduated from the Web Development program at Fanshawe College
+								in 2021, where I learned the foundations of HTML5, CSS3,
+								JavaScript (ES6), LAMP (Linux, Apache, MySQL, PHP), and CMS
+								(WordPress). Since then, I've added React (up to v18),
+								TypeScript, Firebase DB (NoSQL), and Node to my repertoire, as
+								well as Bootstrap, MUI, and Tailwind for styling.
+							</p>
+						</div>
 
-					<div className="text-white">
-						<p className="text-xl text-justify">
-							When I'm not working, I like to be in the sun and stay active by
-							playing volleyball or running. I always try to make time to
-							hangout with friends and family, and its rare that I'm not smiling
-							or laughing. I can't live without music, I love reading
-							science-fiction novels, and I'm a bit of a cinephile.
-						</p>
+						<div className="text-white mb-6">
+							<p className="text-lg md:text-xl md:text-justify">
+								I spend most of my time creating websites, apps, discord bots,
+								you name it, in order to improve my skills as a developer. I
+								gravitate towards building interfaces - I love finding ways to
+								make interactions as intuitive and accessible as possible while
+								maintaining a sleek and thematic style.
+							</p>
+						</div>
+
+						<div className="text-white">
+							<p className="text-lg md:text-xl md:text-justify">
+								When I'm not working, I like to be in the sun and stay active by
+								playing volleyball or running. I always try to make time to
+								hangout with friends and family, and its rare that I'm not
+								smiling or laughing. I can't live without music, I love reading
+								science-fiction novels, and I'm a bit of a cinephile.
+							</p>
+						</div>
 					</div>
 				</div>
+
 				{/* Technology icons section */}
-				<div className="hidden md:block md:w-1/4 md:mt-48 md:px-20 md:ml-16">
+				<div className="hidden lg:block lg:w-1/4 md:mt-48 md:px-20 md:ml-16">
 					<div className="grid grid-cols-2 gap-y-8">
 						{techIcons.map((tech, index) => (
 							<div
@@ -171,6 +188,54 @@ const About = () => {
 							</div>
 						))}
 					</div>
+				</div>
+			</div>
+
+			{/* MOBILE PARAGRAPHS */}
+			<div className="container mx-auto block md:hidden px-4 py-4">
+				<div className="text-white mb-6">
+					<p className="text-lg lg:text-xl md:text-justify">
+						I believe in respecting the user.{" "}
+						<span className="italic decoration-meadow-500 bg-shark-300">
+							Fewer clicks, faster results.
+						</span>{" "}
+						In order to deliver on this principle, I'm driven to discover all
+						that is possible of the tools at my disposal. As a result, and in
+						addition to being naturally curious, I am excited to engage with new
+						technologies and take every opportunity to synthesize new knowledge
+						in order to build the best product imaginable.
+					</p>
+				</div>
+
+				<div className="text-white mb-6">
+					<p className="text-lg md:text-xl md:text-justify">
+						I graduated from the Web Development program at Fanshawe College in
+						2021, where I learned the foundations of HTML5, CSS3, JavaScript
+						(ES6), LAMP (Linux, Apache, MySQL, PHP), and CMS (WordPress). Since
+						then, I've added React (up to v18), TypeScript, Firebase DB (NoSQL),
+						and Node to my repertoire, as well as Bootstrap, MUI, and Tailwind
+						for styling.
+					</p>
+				</div>
+
+				<div className="text-white mb-6">
+					<p className="text-lg md:text-xl md:text-justify">
+						I spend most of my time creating websites, apps, discord bots, you
+						name it, in order to improve my skills as a developer. I gravitate
+						towards building interfaces - I love finding ways to make
+						interactions as intuitive and accessible as possible while
+						maintaining a sleek and thematic style.
+					</p>
+				</div>
+
+				<div className="text-white">
+					<p className="text-lg md:text-xl md:text-justify">
+						When I'm not working, I like to be in the sun and stay active by
+						playing volleyball or running. I always try to make time to hangout
+						with friends and family, and its rare that I'm not smiling or
+						laughing. I can't live without music, I love reading science-fiction
+						novels, and I'm a bit of a cinephile.
+					</p>
 				</div>
 			</div>
 		</section>
